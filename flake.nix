d02f00c9d885c7631b449736ae1e914068de4438
LeixB <abone9999@gmail.com>
2023-03-17 16:17:22 +0100

    flake: fix config

--- flake.nix
+++ flake.nix
@@ -1,4 +1,14 @@
 {
+  nixConfig = {
+    tarball-ttl = 63072000; # 2 years
+    extra-substituters = [
+      "https://tfm.cachix.org/"
+    ];
+    extra-trusted-public-keys = [
+      "tfm.cachix.org-1:XGcdmGOXdUqRzemq7YoN8poxuAMUdnKlJGqzxfoFEWc="
+    ];
+  };
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
@@ -14,10 +24,7 @@
   outputs = { self, nixpkgs, flake-utils, devenv, ... } @ inputs:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-          config.tarball-ttl = 63072000; # 2 years
-        };
+        pkgs = import nixpkgs { inherit system; };
         devenvShell = devenv.lib.mkShell {
           inherit inputs pkgs;
           modules = [ (import ./devenv.nix) ];
