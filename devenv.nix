29571ce94df2e75aa39d805c468f2d28b788105c
LeixB <abone9999@gmail.com>
2023-03-17 16:19:12 +0100

    devenv: pluto as script

--- devenv.nix
+++ devenv.nix
@@ -19,6 +19,8 @@
     curl -f http://127.0.0.1:23119/better-bibtex/export/collection?/1/TFM.biblatex -o "$DEVENV_ROOT/document/biblio.bib" || echo "Is Zotero running?"
   '';
 
+  scripts.pluto.exec = "julia --project=. -e 'using Pkg; Pkg.instantiate(); using Pluto; Pluto.run()'";
+
   enterShell = ''
     task project:$PROJECT summary || echo "No summary available"
     task project:$PROJECT limit:10 next || echo "No tasks found"
@@ -72,5 +74,5 @@
   # https://devenv.sh/processes/
   processes.jupyter.exec = "jupyter lab";
   processes.latexmk.exec = "build-doc -pvc";
-  processes.julia.exec = "julia --project=. -e 'using Pkg; Pkg.instantiate(); using Pluto; Pluto.run()'";
+  processes.pluto.exec = "pluto";
 }
