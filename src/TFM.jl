3879cf7bbd630182d69bf60682fbdddc4d516304
LeixB <abone9999@gmail.com>
2023-04-30 01:30:57 +0200

    julia: workaround to deserialize with older modules

--- src/TFM.jl
+++ src/TFM.jl
@@ -13,6 +13,11 @@ export Resampling
 include("./measures.jl")
 export Measures
 
+# HACK: Export `mse` like this so that deserialization works with machines
+# that were saved before the Measures module was created.
+function mse(ŷ, y) return Measures.mse(ŷ, y) end
+export mse
+
 include("./datasets.jl")
 export DataSets
 
