72be7980b31cc8a18f49582140fce83b757b9257
LeixB <abone9999@gmail.com>
2023-05-01 01:32:20 +0200

    julia: use MLJBase measure

--- src/measures.jl
+++ src/measures.jl
@@ -3,32 +3,34 @@ module Measures
 using MLJBase
 using StatisticalTraits
 
-import MLJBase: Measure, Aggregated
-
-struct MeanSquaredError <: Aggregated end
-
-const MSE = MeanSquaredError
-
-call(::MeanSquaredError, ŷ, y) = (y .- ŷ).^2 |> skipinvalid |> mean
-call(::MeanSquaredError, ŷ, y, w) = (y .- ŷ).^2 .* w |> skipinvalid |> mean
-
-function mse(ŷ, y)
-    return call(MeanSquaredError(), ŷ, y)
-end
-
-function mse(ŷ, y, w)
-    return call(MeanSquaredError(), ŷ, y, w)
-end
-
-const mean_squared_error = mse
+import MLJBase: Measure, Aggregated, @create_aliases, @create_docs, DOC_INFINITE, detailed_doc_string, InfiniteArrMissing
 
 struct MeanSquare <: Aggregated end
 (::MeanSquare)(v) = mean(skipinvalid(v).^2)
 
+struct MeanSquaredError <: Aggregated end
+
 metadata_measure(MeanSquaredError;
                  instances                = ["mse", "mean_squared_error"],
+                 target_scitype           = InfiniteArrMissing,
                  prediction_type          = :deterministic,
                  orientation              = :loss,
                  aggregation              = MeanSquare())
 
-end
+call(::MeanSquaredError, ŷ, y) = (y .- ŷ).^2 |> skipinvalid |> mean
+call(::MeanSquaredError, ŷ, y, w) = (y .- ŷ).^2 .* w |> skipinvalid |> mean
+
+const MSE = MeanSquaredError
+@create_aliases MeanSquaredError
+
+@create_docs(MeanSquaredError,
+body=
+"""
+``\\text{mean squared error} = n^{-1}∑ᵢ|yᵢ-ŷᵢ|^2`` or
+``\\text{mean squared error} = \\frac{∑ᵢwᵢ|yᵢ-ŷᵢ|^2}{∑ᵢwᵢ}``
+""",
+scitype=DOC_INFINITE)
+
+export MeanSquare, MeanSquaredError, MSE, mse, mean_squared_error
+
+end # module Measures
