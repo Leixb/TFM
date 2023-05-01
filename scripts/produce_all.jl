1212d77be1b4e9b1e21f64e4dbf2ae0a261b9a2a
LeixB <abone9999@gmail.com>
2023-05-02 01:33:40 +0200

    julia: investigate how to share data between runs

--- scripts/produce_all.jl
+++ scripts/produce_all.jl
@@ -6,7 +6,7 @@ using Distributed
 using DrWatson
 using ProgressMeter
 
-step = 4.0
+step = 3.0
 
 datasets = filter(DataSets.all) do d
     !(d in [DataSets.mnist, DataSets.cancer])
@@ -14,7 +14,8 @@ end
 
 parameters_common = Dict(
     :dataset => datasets,
-    :cost => 10 .^ (-2:step:6),
+    # :cost => 10 .^ (-2:step:6),
+    :cost => 10 .^ (-2:step:4),
     :epsilon => 10 .^ (-5:step:1),
 )
 
@@ -41,13 +42,15 @@ readline() == "y" || error("Aborted.")
 addprocs(10)
 
 @everywhere begin
-    using TFM.Experiments: EpsilonSVRConfig
+    using TFM.Experiments: EpsilonSVRConfig, LoadedData
     using DrWatson: produce_or_load
+
+    ld = LoadedData()
 end
 
 successes = @showprogress pmap(parameters_all) do params
     ex = EpsilonSVRConfig(;params...)
-    _, file = produce_or_load(ex; loadfile=false)
+    _, file = produce_or_load(ex; cache=ld, loadfile=false)
     1
 end
 
