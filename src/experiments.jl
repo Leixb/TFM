9ac450b30dbc4f1df3b8849756a2256e417259c7
LeixB <abone9999@gmail.com>
2023-05-01 19:20:40 +0200

    julia: experiment produce_or_load

--- src/experiments.jl
+++ src/experiments.jl
@@ -1,10 +1,63 @@
+"""
+# Experiment
+
+This module defines how experiments are run and saved.
+
+Under the hood, it uses [DrWatson](https://juliadynamics.github.io/DrWatson.jl/dev/), which enables
+us to save the results of experiments in a structured way and
+to easily load them back into Julia for further analysis.
+
+## produce_or_load
+
+Thanks to DrWatson, we can use the `produce_or_load` macro to run an experiment
+and save the results to disk. If the experiment has already been run, it will
+load the results from disk instead.
+
+## Workflow
+
+The typical workflow is as follows:
+
+```julia
+ex = EpsilonSVRConfig(DataSets.cpu, kernel=LIBSVM.Kernel.RadialBasis, cost=1.0, gamma=0.0, epsilon=0.1)
+results, file = produce_or_load(ex)
+```
+
+Using `dict_list` we can, generate all the possible combinations of parameters
+for a given experiment:
+
+```julia
+c = Dict(
+    :cost => [0.1, 1.0, 10.0],
+    :gamma => [0.0, 0.1, 1.0],
+    :epsilon => [0.1, 0.2, 0.3]
+)
+grid = map(dict_list(c)) do params
+    EpsilonSVRConfig(;dataset=DataSets.cpu, params...)
+end
+
+results = map(produce_or_load, grid)
+```
+
+Finally, we can make use of the `collect_results` function to collect the
+results of multiple experiments into a single DataFrame:
+
+```julia
+df = collect_results!(datadir())
+```
+
+## References
+
+- [DrWatson: Saving Tools](https://juliadynamics.github.io/DrWatson.jl/dev/save/)
+
+"""
 module Experiments
 
 using Dates
 using MLJ, MLJBase
 using LIBSVM
 
-import DrWatson: allaccess, default_prefix, default_allowed
+using DrWatson
+import DrWatson: allaccess, default_prefix, default_allowed, produce_or_load
 
 import ..DataSets: DataSet, CategoricalDataSet, RegressionDataSet
 import ..Measures: MeanSquaredError
@@ -12,30 +65,6 @@ import ..Models
 
 import ..TFMType
 
-"""
-# Experiment
-
-Abstract type for all experiments.
-
-It is used to store the **metadata**, **parameters** and **results** of an experiment to
-be able to save it into a file and *to store and reproduce it* later.
-
-## Fields
-
-- `date::Date`: the date when the experiment was run.
-- `host::String`: the *hostname* of the machine where the experiment was run.
-
-## Interface
-
-- `run!(experiment::Experiment)`: run the experiment and store the results.
-- `save(experiment::Experiment, filename::String)`: save the experiment to a file.
-- `load(filename::String)::Experiment`: load an experiment from a file.
-
-## References
-
-- [DrWatson: Saving Tools](https://juliadynamics.github.io/DrWatson.jl/dev/save/)
-
-"""
 Base.@kwdef struct ExecutionInfo <: TFMType
     date::Date = Date(Dates.now())
     host::String = gethostname()
@@ -95,10 +124,9 @@ allaccess(svm::SVMConfig) = [ :dataset, :resampling, :measure, model_parameters(
 has_run(ex::SVMConfig) = ex.info isa ExecutionInfo
 _assert_has_run(ex::SVMConfig) = @assert has_run(ex) "$(ex) has not been run yet."
 
-function default_prefix(ex::SVMConfig)
-    _assert_has_run(ex)
-    "svc_$(ex.info.date)"
-end
+default_prefix(::SVMConfig) = "SVM"
+default_prefix(::EpsilonSVRConfig) = "SVR"
+default_prefix(::SVCConfig) = "SVC"
 
 default_allowed(::TFMType) = (
     Real, String, Symbol, TimeType, Kernel.KERNEL, DataSet, ResamplingStrategy, MLJBase.Measure
@@ -124,6 +152,24 @@ function run!(svm::SVMConfig)::PerformanceEvaluation
     svm.info = ExecutionInfo(;duration)
 
     return svm.result
+end
+
+default_savefile(svm::SVMConfig) = datadir(savename(svm, "jld2"))
+
+function save(svm::SVMConfig; filename::String=default_savefile(svm))
+    _assert_has_run(svm)
+    @info "Saving to $(filename)"
+    tagsave(filename, struct2dict(svm))
+end
+
+function load(svm::SVMConfig; filename::String=default_savefile(svm))
+    @info "Loading $(filename)"
+    wload(filename)
+end
+
+produce_or_load(svm::SVMConfig) = produce_or_load(svm; filename=default_savefile(svm)) do ex
+    run!(ex)
+    struct2dict(ex)
 end
 
 end # module Experiments
