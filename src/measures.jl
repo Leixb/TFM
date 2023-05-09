4ff043279bde4edffde79215ba3a38f2a9e949af
LeixB <abone9999@gmail.com>
2023-05-09 23:21:44 +0200

    julia: add nRMSE and nMSE measures

--- src/measures.jl
+++ src/measures.jl
@@ -1,7 +1,7 @@
 module Measures
 
 using StatisticalTraits
-import MLJBase: @create_aliases, @create_docs, Aggregated, DOC_INFINITE, InfiniteArrMissing, Mean, Measure, detailed_doc_string, mean, metadata_measure, skipinvalid
+import MLJBase: @create_aliases, @create_docs, Aggregated, DOC_INFINITE, InfiniteArrMissing, Mean, Measure, detailed_doc_string, mean, metadata_measure, skipinvalid, std, rmse
 
 # ----------------------------------------------------------------
 # MeanSquaredError
@@ -36,4 +36,57 @@ Useful for printing the name of the measure in a table or in a filename.
 """
 Base.show(io::IO, measure::Measure) = print(io, instances(typeof(measure))[1])
 
+# ----------------------------------------------------------------
+# NormalizedMeanSquaredError
+
+struct NormalizedMeanSquaredError <: Aggregated end
+
+metadata_measure(NormalizedMeanSquaredError;
+                 instances                = ["nmse", "normalized_mean_squared_error"],
+                 target_scitype           = InfiniteArrMissing,
+                 prediction_type          = :deterministic,
+                 orientation              = :loss,
+                 aggregation              = Mean())
+
+const nMSE = NormalizedMeanSquaredError
+@create_aliases NormalizedMeanSquaredError
+
+@create_docs(NormalizedMeanSquaredError,
+body = """
+``\\text{normalized mean squared error} = \\frac{n^{-1}∑ᵢ|yᵢ-ŷᵢ|^2}{var(ŷ)}`` or
+``\\text{normalized mean squared error} = \\frac{∑ᵢwᵢ|yᵢ-ŷᵢ|^2}{var(ŷ)∑ᵢwᵢ}``
+""",
+scitype = DOC_INFINITE)
+
+(::NormalizedMeanSquaredError)(ŷ, y) = mse(ŷ, y) / (std(y)^2)
+(::NormalizedMeanSquaredError)(ŷ, y, w) = mse(ŷ, y, w) / (std(y)^2)
+
+export NormalizedMeanSquaredError, nMSE, nmse, normalized_mean_squared_error
+
+# ----------------------------------------------------------------
+# NormalizedRootMeanSquaredError
+
+struct NormalizedRootMeanSquaredError <: Aggregated end
+
+metadata_measure(NormalizedRootMeanSquaredError;
+                 instances                = ["nrmse", "normalized_root_mean_squared_error"],
+                 target_scitype           = InfiniteArrMissing,
+                 prediction_type          = :deterministic,
+                 orientation              = :loss,
+                 aggregation              = RootMeanSquare())
+
+const nRMSE = NormalizedRootMeanSquaredError
+@create_aliases NormalizedRootMeanSquaredError
+
+@create_docs(NormalizedRootMeanSquaredError,
+body = """
+``\\text{normalized root mean squared error} = \\frac{\\sqrt{n^{-1}∑ᵢ|yᵢ-ŷᵢ|^2}}{std(y)}`` or
+``\\text{normalized root mean squared error} = \\frac{\\sqrt{\\frac{∑ᵢwᵢ|yᵢ-ŷᵢ|^2}{∑ᵢwᵢ}}}{std(y)}``
+""",
+scitype = DOC_INFINITE)
+
+(::NormalizedRootMeanSquaredError)(ŷ, y) = rmse(ŷ, y) / std(y)
+(::NormalizedRootMeanSquaredError)(ŷ, y, w) = rmse(ŷ, y, w) / std(y)
+
+export NormalizedRootMeanSquaredError, nRMSE, nrmse, normalized_root_mean_squared_error
 end # module Measures
