3199a49be797a3f63a1d6494b2942f22f010a686
LeixB <abone9999@gmail.com>
2023-05-25 11:23:47 +0200

    scripts: run_till_success any script / command

--- scripts/run_till_success.sh
+++ scripts/run_till_success.sh
@@ -8,11 +8,14 @@
 # multiple times until it succeeds. Since it saves the results to
 # disk it won't repeat previous computations.
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+PROG="${1:-"$SCRIPT_DIR/produce_all.jl"}"
+
 MAX_TRIES=10
 
 for i in $(seq 1 $MAX_TRIES); do
     echo "Attempt $i" | notify
-    if ./scripts/produce_all.jl;
+    if $PROG;
     then
         echo "Success" | notify
         exit 0
