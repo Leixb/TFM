fae3acefcb42cf9f588c9bda4d0c96a9b9d3b4c5
LeixB <abone9999@gmail.com>
2023-09-04 00:18:58 +0200

    data: add adult and metadata

--- nix/datasets.nix
+++ nix/datasets.nix
@@ -2,18 +2,25 @@
 , fetchurl
 , fetchzip
 , linkFarm
+, writeText
 }:
 let
   datasets = builtins.fromTOML (builtins.readFile ../datasets.toml);
   drvList = lib.mapAttrsToList
     (name: data:
+      let download_data = lib.filterAttrs (k: _: k != "meta") data; in
       {
         inherit name;
         path = (if lib.hasSuffix "gz" data.url then fetchTarball else
-        (if lib.hasSuffix "zip" data.url then fetchzip else fetchurl)) data;
+        (if lib.hasSuffix "zip" data.url then fetchzip else fetchurl)) download_data;
       }
     )
     datasets
   ;
+  datasets_meta = builtins.toJSON (lib.mapAttrs (name: data: data.meta) datasets);
+  datasets_meta_file = rec {
+    name = "metadata.json";
+    path = writeText name datasets_meta;
+  };
 in
-linkFarm "datasets" drvList
+linkFarm "datasets" (drvList ++ [ datasets_meta_file ])
