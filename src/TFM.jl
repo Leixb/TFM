8b91fe8f9bc871931b26cdabd60379c936d1d769
LeixB <abone9999@gmail.com>
2023-09-25 17:32:12 +0200

    fix: module include order

--- src/TFM.jl
+++ src/TFM.jl
@@ -38,10 +38,10 @@ export Models
 include("./experiments.jl")
 export Experiments
 
+include("./benchmark.jl")
+
 # only if we are in an interactive session
 include("./plots.jl")
-
-include("./benchmark.jl")
 
 # HACK: Export `mse` like this so that deserialization works with machines
 # that were saved before the Measures module was created.
